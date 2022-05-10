6951d79a3661611b21750fa0afd0bef4c5274b00
thakurSaad <thakursaad613@gmail.com>
2022-05-11 01:25:46 +0600

    module 71 done

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./Banner.css";
 import chair from "../../assets/images/chair.png";
 import Button from "../Shared/Button";
+import bgChair from "../../assets/images/bg.png";
 
 const Banner = () => {
   return (
-    <div className="hero min-h-screen px-1">
+    <div
+      style={{
+        background: `url(${bgChair})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'repeat-y'
+      }}
+      className="hero min-h-screen px-1"
+    >
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
           src={chair}
@@ -13,7 +21,7 @@ const Banner = () => {
           alt="banner"
         />
         <div>
-          <h1 className="text-5xl font-bold pr-8">
+          <h1 className="text-5xl font-bold mt-8 md:mt-0 pr-8">
             Your New Smile Starts Here
           </h1>
           <p className="py-6">
