a6266f0eee72373d5086bc9b17a40fb34180c172
thakurSaad <thakursaad613@gmail.com>
2022-05-10 17:02:22 +0600

    banner done without background image

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,21 +3,22 @@ import chair from "../../assets/images/chair.png";
 
 const Banner = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen px-1">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
+          style={{ maxWidth: "594px" }}
           src={chair}
-          className="max-w-sm rounded-lg shadow-2xl"
+          className="max-w-sm rounded-lg shadow-2xl "
           alt="banner"
         />
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-5xl font-bold pr-8">Your New Smile Starts Here</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary font-bold text-white">GET STARTED</button>
         </div>
       </div>
     </div>
