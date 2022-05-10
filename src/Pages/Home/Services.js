f053b278981f212d0ad9852830117e9cae2118ca
thakurSaad <thakursaad613@gmail.com>
2022-05-10 20:00:07 +0600

    common Button componetnt

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -4,6 +4,7 @@ import fluoride from "../../assets/images/fluoride.png";
 import cavity from "../../assets/images/cavity.png";
 import whitening from "../../assets/images/whitening.png";
 import treatment from "../../assets/images/treatment.png";
+import Button from "../Shared/Button";
 
 const Services = () => {
   const services = [
@@ -33,32 +34,30 @@ const Services = () => {
         Our Services
       </h3>
       <h3 className="text-center text-2xl">Services We Provide</h3>
-      <div className="grid justify-items-center lg:grid-cols-3 gap-8 my-16">
+      <div className="grid justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-8 my-16">
         {services.map((service) => (
           <Service key={service._id} service={service}></Service>
         ))}
       </div>
-      
+
       <div className="lg:w-4/5 mx-auto">
-        <div class="hero min-h-screen">
-          <div class="hero-content flex-col lg:flex-row">
+        <div className="hero min-h-screen justify-center">
+          <div className="hero-content flex-col lg:flex-row">
             <img
               src={treatment}
-              class="max-w-sm rounded-lg shadow-2xl lg:w"
+              className="max-w-sm rounded-lg shadow-2xl w-80 lg:w-full"
               alt="Exceptional Dental Care"
             />
-            <div>
-              <h1 class="text-5xl font-bold">
+            <div className="lg:px-12">
+              <h1 className="text-5xl font-bold">
                 Exceptional Dental Care, on Your Terms
               </h1>
-              <p class="py-6">
+              <p className="py-6">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
                 assumenda excepturi exercitationem quasi. In deleniti eaque aut
                 repudiandae et a id nisi.
               </p>
-              <button class="btn btn-primary bg-gradient-to-r from-secondary to-primary">
-                Get Started
-              </button>
+              <Button></Button>
             </div>
           </div>
         </div>
---
+++ src/Pages/Shared/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary font-bold text-white">
+        GET STARTED
+      </button>
+    </div>
+  );
+};
+
+export default Button;
