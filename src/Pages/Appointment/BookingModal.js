77f099b35e32c2cbab9c7f660848321f6f4c89a3
thakurSaad <thakursaad613@gmail.com>
2022-05-11 18:34:54 +0600

    form into modal with treatment data

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BookingModal = ({ treatment }) => {
+const BookingModal = ({ treatment, date }) => {
   const { name, slots } = treatment;
 
   return (
@@ -14,16 +14,40 @@ const BookingModal = ({ treatment }) => {
           >
             ✕
           </label>
-          <h3 className="font-bold text-lg">Booking For {name}</h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div className="modal-action">
-            <label htmlFor="booking-modal" className="btn">
-              Yay!
-            </label>
-          </div>
+          <form className="text-center">
+            <h3 className="font-bold text-lg">{name}</h3>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-lg mt-4"
+            />
+            <input
+              type="text"
+              value={slots[0]}
+              disabled
+              className="input input-bordered w-full max-w-lg mt-4"
+            />
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="input input-bordered w-full max-w-lg mt-4"
+            />
+            <input
+              type="Number"
+              placeholder="Phone Number"
+              className="input input-bordered w-full max-w-lg mt-4"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered w-full max-w-lg mt-4"
+            />
+            <input
+              type="submit"
+              value="SUBMIT"
+              className="bg-accent text-white font-semibold input w-full max-w-lg mt-4"
+            />
+          </form>
         </div>
       </div>
     </div>
