4b5bc6b42d19b627be98bf084f1b0fb197eb329f
thakurSaad <thakursaad613@gmail.com>
2022-05-11 15:28:17 +0600

    data load from github raw | 72-3

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,12 +1,24 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AvailableAppointments = ({ date }) => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/doctors-portal-client-module-72/main/public/services.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  console.log(services);
+
   return (
     <section className="my-24">
       <h3 className="text-center font-bold text-secondary text-xl uppercase mb-24">
         Available Appointments on {format(date, "PP")}
       </h3>
+      {}
     </section>
   );
 };
