bd677469c647064e24e50c3a2b152ce8482cb6ed
thakurSaad <thakursaad613@gmail.com>
2022-05-11 17:59:00 +0600

    modal button and show data on modal | 72-5

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 const BookingModal = ({ treatment }) => {
+  const { name, slots } = treatment;
+
   return (
     <div>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
+        <div className="modal-box relative">
+          <label
+            htmlFor="booking-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="font-bold text-lg">Booking For {name}</h3>
           <p className="py-4">
             You've been selected for a chance to get one year of subscription to
             use Wikipedia for free!
