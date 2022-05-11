4bbafe490ccd7a53e93f3fba79136da3049fa3ff
thakurSaad <thakursaad613@gmail.com>
2022-05-11 11:21:13 +0600

    Availabel appointments comp

---
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AvailableAppointments = () => {
+  return (
+    <div>
+      <h3 className="text-center font-bold text-secondary text-xl uppercase">
+        Available Appointments on April 30, 2022Our Services
+      </h3>
+    </div>
+  );
+};
+
+export default AvailableAppointments;
