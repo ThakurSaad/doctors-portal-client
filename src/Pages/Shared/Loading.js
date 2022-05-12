8283eafc9733b8463597b46308c837f00c0ae4b0
thakurSaad <thakursaad613@gmail.com>
2022-05-12 18:49:18 +0600

    Interesiting Loading spinner

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="text-center">
-      <button class="btn loading">loading</button>
-    </div>
+    <section className="h-screen flex items-center justify-center ">
+      <div class="lg:hidden">
+        <div class="w-16 h-16 border-t-2 border-b-2 border-accent rounded-full animate-spin"></div>
+      </div>
+      <div class="hidden lg:block">
+        <div class="w-40 h-40 border-t-4 border-b-4 border-accent rounded-full animate-spin"></div>
+      </div>
+    </section>
   );
 };
 
