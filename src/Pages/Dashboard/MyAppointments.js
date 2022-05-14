7994b71910bc0ca610a7ec15c9ca917a267456a6
thakurSaad <thakursaad613@gmail.com>
2022-05-14 23:06:18 +0600

    load data based on query parameter

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyAppointments = () => {
   const [appointments, setAppointments] = useState([]);
-  const [user] = useAuthState();
+  const [user] = useAuthState(auth);
 
-  console.log(user);
   useEffect(() => {
-    // fetch(`http://localhost:4000/booking?patient=${}`)
-  },[])
-
+    if (user) {
+      fetch(`http://localhost:4000/booking?patient=${user?.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+        console.log(user);
+    }
+  }, [user]);
   return (
     <div>
-      <h3 className="text-accent text-4xl uppercase">
-        My Appointments
-      </h3>
+      <h3 className="text-accent text-4xl uppercase">My Appointments : {appointments.length} </h3>
     </div>
   );
 };
