0b5dfb17afe620e8f004ef9d011c17d590b46b9c
thakurSaad <thakursaad613@gmail.com>
2022-05-15 23:41:48 +0600

    useAdmin hook

---
+++ src/Pages/hooks/useAdmin.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const useAdmin = (user) => {
+  const [admin, setAdmin] = useState(false);
+  
+  useEffect(() => {
+    const email = user?.email;
+
+    if (email) {
+      fetch(`http://localhost:4000/user/${email}`, {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setAdmin(data.admin);
+        });
+    }
+  }, [user]);
+  return [admin];
+};
+
+export default useAdmin;
