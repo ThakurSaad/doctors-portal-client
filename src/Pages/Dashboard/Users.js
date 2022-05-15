70e6f2ec4f0271b182b48010ffe959337a44cac1
thakurSaad <thakursaad613@gmail.com>
2022-05-15 19:52:42 +0600

    data load with react query and used isLoading

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const Users = () => {
+  const { data: users, isLoading } = useQuery("users", () =>
+    fetch("http://localhost:4000/user").then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <section>
-      <h3 className="text-accent text-4xl uppercase ml-8">All Users</h3>
+      <h3 className="text-accent text-4xl uppercase ml-8">
+        All Users : {users.length}
+      </h3>
     </section>
   );
 };
