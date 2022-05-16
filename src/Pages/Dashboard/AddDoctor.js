b8d07326f16be8ff15ec412e73a1eb45fd86ac1a
thakurSaad <thakursaad613@gmail.com>
2022-05-16 23:57:24 +0600

    signUp like form in add doctor

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -4,6 +4,105 @@ const AddDoctor = () => {
   return (
     <section>
       <h3 className="text-accent text-4xl uppercase ml-8">Add A New Doctor</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text font-semibold">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Your name"
+            className="input input-bordered w-full max-w-xs"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "Name is required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.name?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </label>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text font-semibold">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Your email"
+            className="input input-bordered w-full max-w-xs"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+              pattern: {
+                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                message: "Provide a valid email",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.email?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span className="label-text-alt text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+          </label>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text font-semibold">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered w-full max-w-xs"
+            {...register("password", {
+              required: {
+                value: true,
+                message: "Password is required",
+              },
+              minLength: {
+                value: 6,
+                message: "Must be 6 characters long or higher",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.password?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.password.message}
+              </span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span className="label-text-alt text-red-500">
+                {errors.password.message}
+              </span>
+            )}
+          </label>
+        </div>
+        {singInError}
+        <input className="btn w-full max-w-xs" type="submit" value="SIGN UP" />
+        <p className="text-center p-1">
+          <small>
+            Already have an account ?{" "}
+            <Link className="text-primary font-semibold" to="/login">
+              Login
+            </Link>
+          </small>
+        </p>
+      </form>
     </section>
   );
 };
