4b77f54f794e161dd181d00205e3fe7382e40336
thakurSaad <thakursaad613@gmail.com>
2022-05-17 14:38:12 +0600

    show doctors in tabular format

--- src/Pages/Dashboard/ManageDoctor.js
+++ src/Pages/Dashboard/ManageDoctor.js
@@ -18,6 +18,22 @@ const ManageDoctor = () => {
     <div>
       <h3 className="text-accent text-4xl uppercase ml-8">
         Manage Doctor : {doctors.length}
+        <div class="overflow-x-auto">
+          <table class="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Avatar</th>
+                <th>Name</th>
+                <th>Speciality</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+          </table>
+        </div>
       </h3>
     </div>
   );
