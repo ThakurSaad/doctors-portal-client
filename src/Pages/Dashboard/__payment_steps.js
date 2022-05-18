bfc888abfa55365fb29dbcbdea49a6de5cc1acb5
thakurSaad <thakursaad613@gmail.com>
2022-05-18 20:30:19 +0600

    Show card error on UI

---
+++ src/Pages/Dashboard/__payment_steps.js
@@ -0,0 +1,11 @@
+/* 
+1. install stripe js
+2. create a stripe account 
+3. get publishable key pk__
+4. create element wrapper using publishable key 
+5. create checkout form using cardElement , useStripe, useElement
+6. get card elements info (credit card info)
+*/
+/* 
+7. get credit card info/error
+*/
