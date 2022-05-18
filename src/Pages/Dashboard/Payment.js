fbaa09b8d41d0035820e4373bacf723583d7c347
thakurSaad <thakursaad613@gmail.com>
2022-05-18 16:17:20 +0600

    route with id UI

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,16 +1,49 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import Loading from "../Shared/Loading";
 
 const Payment = () => {
   const { appointmentId } = useParams();
+  const url = `http://localhost:4000/booking/${appointmentId}`;
+
+  const { data: appointment, isLoading } = useQuery(
+    ["booking", appointmentId],
+    () =>
+      fetch(url, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }).then((res) => res.json())
+  );
+  const { patientName, treatment, date, slot, price } = appointment || "";
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
-    <div>
-      <h3 className="text-accent text-4xl uppercase ml-8">
-        Please Pay for &nbsp;
-        <span className="font-semibold text-purple-500">{appointmentId}</span>
-      </h3>
-    </div>
+    <section className="mx-8 my-8">
+      <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+        <div className="card-body">
+          <h2 className="card-title">Pay For {treatment}</h2>
+          <p>
+            We will see you on &nbsp;
+            <span className="text-orange-700 font-semibold">{date}</span>&nbsp;
+            at&nbsp;
+            {slot}
+          </p>
+          <p>Please pay ${price}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+        <div className="card-body"></div>
+      </div>
+    </section>
   );
 };
 
